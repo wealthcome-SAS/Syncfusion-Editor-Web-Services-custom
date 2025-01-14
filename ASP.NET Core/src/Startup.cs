dc0f4d600d414c0f1048aa5e443407caa5ac52d6
Ugolin O. <hello@ugolin-olle.com>
2025-01-14 23:45:56 +0700

    feat(core): update cors due to some tickets

--- ASP.NET Core/src/Startup.cs
+++ ASP.NET Core/src/Startup.cs
@@ -74,7 +74,9 @@ namespace EJ2APIServices
                                 "http://localhost:5173",
                                 "http://127.0.0.1:5173",
                                 "http://localhost:5174",
-                                "http://127.0.0.1:5174"
+                                "http://127.0.0.1:5174",
+                                "https://pro.preprod.wealthcome.fr",
+                                "https://pro.wealthcome.fr"
                             )
                             .AllowAnyMethod()
                             .AllowAnyHeader()
