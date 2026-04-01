537030e049dff6f6cd1f2ee3ab63d85181afc630
AxelMth <aujoulator@hotmail.fr>
2026-04-01 17:44:46 +0200

    update CORS policy

--- ASP.NET Core/src/Startup.cs
+++ ASP.NET Core/src/Startup.cs
@@ -78,6 +78,7 @@ namespace EJ2APIServices
                                 "https://pro.preprod.wealthcome.fr",
                                 "https://pro.wealthcome.fr",
                                 "https://v2.pro.preproduction.wealthcome.fr",
+                                "https://pro.staging.aws.wealthcome.fr",
                                 "https://pro.preproduction.aws.wealthcome.fr",
                                 "https://pro.production.aws.wealthcome.fr"
                             )
